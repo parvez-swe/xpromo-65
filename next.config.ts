17e258791ec8e433cba57c7b7bdfdc7dd2b28497
parvezswe <mdparvezmusharaf2@gmail.com>
2025-03-24 00:50:00 +0600

    update shop by industry logo config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    // Add SVGR loader for SVG component support
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
